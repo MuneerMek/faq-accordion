e15fec1ec9277bfcf186983bcac4254b03db490c
debian-pc <mmekraz781@proton.me>
2024-04-02 21:14:46 -0400

    Finalized page

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const buttons = document.querySelectorAll(".button");
+const headerImg = document.querySelectorAll(".header-img");
 const infos = document.querySelectorAll(".info");
 const plus = "assets/images/icon-plus.svg";
 const minus = "assets/images/icon-minus.svg";
 
-buttons.forEach((button, index) => {
-  button.addEventListener("click", () => {
+headerImg.forEach((faqItem, index) => {
+  faqItem.addEventListener("click", () => {
     const info = infos[index];
+    const button = buttons[index];
     if (info) {
       info.hidden = !info.hidden;
       if (button.src.includes(plus)) {
